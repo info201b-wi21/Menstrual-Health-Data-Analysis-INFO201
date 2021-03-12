9fa3018632d0ad40dc1e97a83ea1a676e42c9852
Franchezca <>
2021-03-11 18:07:33 -0800

    Finished plot of Analysis Q2

--- my_server.R
+++ my_server.R
@@ -3,6 +3,8 @@ library('tidyr')
 source('analysis.R')
 
 my_server <- function(input, output) {
+  
+  #
   output$analysis_q1 <- renderPlot({
     q1_data <- preg_comp_by_state_df %>% 
       filter(Complications == input$comp)
@@ -15,6 +17,22 @@ my_server <- function(input, output) {
       scale_fill_brewer(palette="Dark2") +
       theme(axis.text.x = element_text(angle = 90))
     
+    plot
+  })
+  
+  # Plot for analysis question 2
+  output$analysis_q2 <-renderPlot({
+    
+    q2_data <- menstrual_budget_df %>% 
+      select(States,input$year)
+    
+    plot <- ggplot(data=q2_data, aes(x="", y=pull(q2_data,input$year), fill=States)) +
+      geom_bar(stat = 'identity') +
+      coord_polar('y', start=0) +
+      theme_void() +
+      labs(
+        title = paste('Distribution of State Menstrual Health Budget in ', input$year)
+      )
     plot
   })
 }
